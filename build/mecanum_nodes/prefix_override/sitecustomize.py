b3ca8ce4d2393ee3ceffd5f39277149700092d2b
Sheitej <sheitejsharma@gmail.com>
2025-04-12 22:09:39 -0400

    Cleanup

--- build/mecanum_nodes/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sheitej/sherlock_ros/install/mecanum_nodes'
--- sherlock_ros-main_Cpp/build/mecanum_nodes/prefix_override/sitecustomize.py
+++ sherlock_ros-main_Cpp/build/mecanum_nodes/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sheitej/Workspaces/sherlock_ros-main_Cpp/install/mecanum_nodes'
+    sys.prefix = sys.exec_prefix = '/home/sheitej/sherlock_ros/sherlock_ros-main_Cpp/install/mecanum_nodes'
